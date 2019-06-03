dfb61705a64672548372e3c84f7ea418e46b804f
MaDDDDDoG <yo8787gg3be0@gmail.com>
2019-06-03 18:20:00 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    static public void main(String[] args){
+        UI a = new UI();
+        a.setVisible(true);
+    }
+}
+
---
+++ src/SwapPair.java
@@ -0,0 +1,8 @@
+public class SwapPair {
+    public int first, second;
+
+    public SwapPair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
