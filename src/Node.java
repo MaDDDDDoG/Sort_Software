76ee2ae311f7d89861e3ce34d50c1c37121dd47b
MaDDDDDoG <yo8787gg3be0@gmail.com>
2019-06-05 23:22:46 +0800

    2

--- src/Node.java
+++ src/Node.java
@@ -1,33 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import javax.swing.*;
 
-public class Node {
-    private String[] property;
-    private String object;
-    static private String class_name;
-    static private HashMap<String, Integer> item_index;
-    static private HashMap<String, String> item_type;
-    static private int property_size;
+public class Node extends JLabel{
 
-    public Node(String name, List<String> property){
-        object = name;
-        this.property = new String[property_size];
-
-        for(int i=0;i<property_size;i++){
-            this.property[i] = property.get(i);
-        }
-    }
-
-    static public void initialize_Node(String name, List<String> item_n, List<String> item_t){
-        class_name = name;
-        property_size = item_n.size();
-        item_index = new HashMap<>();
-        item_type = new HashMap<>();
-
-        for(int i=0;i<property_size;i++){
-            item_index.put(item_n.get(i), i);
-            item_type.put(item_n.get(i), item_t.get(i));
-        }
-    }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,88 @@
 import javax.swing.*;
+import java.awt.*;
+
 
 public class UI extends JFrame{
+    protected Container cp = getContentPane();
+    protected JToolBar preceding, processing;
+    protected JComboBox select_type, select_datafrom, select_move, select_contour, select_sort;
+    protected JButton finish, next, back, clear;
+    protected JLabel test;
+    protected JPanel node_panel;
 
+    public UI(){
+        super("XXX");
+        setBounds(300, 300, 1000, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        setPreceding();
+        setProcessing();
+        setNode();
+    }
+
+    private void setNode(){
+        node_panel = new JPanel();
+        node_panel.setBackground(Color.CYAN);
+
+        cp.add(node_panel, BorderLayout.CENTER);
+        node_panel.setLayout(null);
+    }
+
+    private void setPreceding(){
+        preceding = new JToolBar();
+
+        select_datafrom = new JComboBox();
+        select_datafrom.addItem("UI input");
+        select_datafrom.addItem("File");
+        select_datafrom.addItem("Database");
+        select_datafrom.addItem("Random");
+        preceding.add(select_datafrom, new CardLayout());
+
+        select_type = new JComboBox();
+        select_type.addItem("Integer");
+        select_type.addItem("Double");
+        select_type.addItem("String");
+        select_type.addItem("Object");
+        preceding.add(select_type);
+
+        select_sort = new JComboBox();
+        select_sort.addItem("Insertion Sort");
+        select_sort.addItem("Selection Sort");
+        select_sort.addItem("Bubble Sort");
+        select_sort.addItem("Merge Sort");
+        select_sort.addItem("Quick Sort");
+        preceding.add(select_sort);
+
+        select_contour = new JComboBox();
+        select_contour.addItem("Circle");
+        select_contour.addItem("Square");
+        preceding.add(select_contour);
+
+        select_move = new JComboBox();
+        select_move.addItem("Parabola");
+        select_move.addItem("Line");
+        preceding.add(select_move);
+
+
+        cp.add(preceding, BorderLayout.NORTH);
+    }
+
+    private void setProcessing(){
+        processing = new JToolBar();
+
+        finish = new JButton("Finish");
+        processing.add(finish);
+
+        next = new JButton("Next");
+        processing.add(next);
+
+        back = new JButton("Back");
+        processing.add(back);
+
+        clear = new JButton("Clear");
+        processing.add(clear);
+
+        cp.add(processing, BorderLayout.SOUTH);
+    }
 }
